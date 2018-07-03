f86ceb1a815b982239953d8d004267afb9beb292
Tim Vass <merle.vivo@gmail.com>
2018-07-03 11:47:07 +0200

    Create exercise0.c

---
+++ week1/exercise0.c
@@ -0,0 +1,26 @@
+// You need to design a half-pyramide from "a" letters, height of the pyramide is 3
+//
+//a   -> 1x"a" on row no.0 -> pattern: number of "a"-s = row no.+1
+//aa  -> 2x"a" on row no.1 -> pattern: number of "a"-s = row no.+1
+//aaa -> 3x"a" on row no.2 -> pattern: number of "a"-s = row no.+1
+
+#include <stdio.h>
+
+int main(void)
+{
+  //Here you can write the first loop which prompts user for a number. I write here just the result
+  int height = 4;
+  
+  //Print rows with a loop
+  for (int row = 0; row < height; row++)
+  {
+    //Print the "a"-s
+    for (int a = 0; a < ( row + 1); a++)
+    {
+      printf("a");
+    }
+    
+    //Print new line
+    printf("\n");
+  }
+}
